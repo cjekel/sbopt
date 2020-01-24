3a5dea37c5d02963f7264f14d527a183fdc0481e
cjekel <cjekel@gmail.com>
2020-01-24 09:09:34 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import io
+from setuptools import setup
+
+setup(
+    name='sbopt',
+    version='0.0.1',
+    author='Charles Jekel',
+    author_email='cjekel@gmail.com',
+    packages=['sbopt'],
+    url='https://github.com/cjekel/sbopt',
+    license='MIT License',
+    description='sbopt: Simple Surrogate-based optimization algorithm',
+    long_description=io.open('README.md', encoding='utf8').read(),
+    long_description_content_type='text/markdown',
+    platforms=['any'],
+    install_requires=[
+        "numpy >= 1.14.0",
+        "scipy >= 0.19.0",
+        "pyDOE >= 0.3.8",
+        "setuptools >= 38.6.0",
+    ],
+)
