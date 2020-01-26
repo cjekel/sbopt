5bb1a3cf1dbc98889336c556c1985f1bf8588375
cjekel <cjekel@gmail.com>
2020-01-26 10:07:41 -0800

    add holdertable function

--- examples/holdertable.py
+++ examples/holdertable.py
@@ -1,25 +1,18 @@
 import numpy as np
 import sbopt
 
-a = 1.0
-b = 5.1 / (4.0*np.pi**2)
-c = 5.0 / np.pi
-r = 6.0
-s = 10.0
-t = 1.0 / (8.0*np.pi)
+
 
 
 def my_fun(x):
-    # define the Branin-Hoo function to minmize
-    A = a*(x[1] - (b*x[0]**2) + c*x[0] - r)**2
-    B = s*(1.0 - t)*np.cos(x[0])
-    return A + B + s
+    # define the Holder table function 
+    A = np.exp(np.abs(1.0 - (np.sqrt(x[0]**2 + x[1]**2) / np.pi)))
+    return -np.abs(np.sin(x[0])*np.cos(x[1])*A)
 
 
 bounds = np.zeros((2, 2))
-bounds[0, 0] = -5.0
-bounds[0, 1] = 10.0
-bounds[1, 1] = 15.0
+bounds[:, 0] = -10.0
+bounds[:, 1] = 10.0
 
 # set random seed for reproducibility
 np.random.seed(1234124)
